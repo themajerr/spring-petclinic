7562d6eaa4299214e260d0dc5e6ada580dae37a1
themajerr <93267210+themajerr@users.noreply.github.com>
2023-07-12 14:53:27 +0200

    Update main.tf - UNSAFE

--- main.tf
+++ main.tf
@@ -119,7 +119,10 @@ resource "google_sql_database_instance" "sql_instance" {
       }
       ip_configuration {
         ipv4_enabled = true
-       
+        authorized_networks {
+          name            = "all just for test"
+          value           = "0.0.0.0/0"
+        }
       }
     }
 }
